3cc1ed3a1de9f46f69b763a6a1651c5dc8417460
Daniel Vu <evanidul@gmail.com>
2014-01-15 23:31:18 -0800

    formatting create, add comments, plan out student form

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,27 +1,29 @@
 class ChargesController < ApplicationController
 
-def new
-end
+	def new
+	end
 
-def create
-  # Amount in cents
-  @amount = 500
+	# <%= form_tag charges_path do %> in new.html.erb routes to the Charges controller.  Because it's a form, the default
+	# method is a POST, which maps to the 'create' method.  If you run 'rake routes' you can see this automatic mapping.
+	def create
+		# Amount in cents ($5.00)
+	  	@amount = 500
 
-  customer = Stripe::Customer.create(
-    :email => 'example@stripe.com',
-    :card  => params[:stripeToken]
-  )
+		customer = Stripe::Customer.create(
+			:email => 'example@stripe.com',
+			:card  => params[:stripeToken]
+		)
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
+		charge = Stripe::Charge.create(
+			:customer    => customer.id,
+			:amount      => @amount,
+			:description => 'Rails Stripe customer',
+			:currency    => 'usd'
+		)
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to charges_path
-end
+		rescue Stripe::CardError => e
+	  		flash[:error] = e.message
+		  	redirect_to charges_path
+	end
 
 end
