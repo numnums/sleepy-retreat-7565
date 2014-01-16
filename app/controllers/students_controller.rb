73ab24338b29a84f47a560752c045ffc1b05edc7
Daniel Vu <evanidul@gmail.com>
2014-01-16 05:23:22 -0800

    Add student model class and basic forms

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,21 @@
+class StudentsController < ApplicationController
+	
+	def new
+	end
+
+	def create
+  		@student = Student.new(student_params)
+  		@student.save
+  		redirect_to @student
+  	end
+
+  	def show
+  		@student = Student.find(params[:id])
+  	end
+
+  	private
+  		def student_params
+  			params.require(:student).permit(:firstname, :lastname, :gender)
+  		end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ PBNC::Application.routes.draw do
 
   root 'welcome#index'
   resources :charges
+  resources :students
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
