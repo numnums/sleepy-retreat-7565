92e6abe8b5c54230f9c8148223d7dc9187721d41
Daniel Vu <evanidul@gmail.com>
2014-01-23 00:07:16 -0800

    edit student forms

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,22 @@ class StudentsController < ApplicationController
 	def new
 	end
 
+  #shows edit form
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  #processes update
+  def update
+    @student = Student.find(params[:id])
+   
+    if @student.update(params[:student].permit(:firstname, :lastname, :gender))
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
 	def create
   		@student = Student.new(student_params)
   		@student.save
