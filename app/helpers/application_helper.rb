2cbfce1fe8e582fc116a1efd576c9e88d1d03718
Collin Oguro <collin@gurodesign.com>
2014-08-20 22:45:48 -0700

    updating js, adding routing helper for favicon, added footer link styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
 end
+
+# File actionpack/lib/action_view/helpers/asset_tag_helper.rb, line 166
+def favicon_link_tag(source='favicon.ico', options={})
+  tag('link', {
+    :rel  => 'shortcut icon',
+    :type => 'image/vnd.microsoft.icon',
+    :href => path_to_image(source)
+  }.merge!(options.symbolize_keys))
+end
